4612b75a41942559770c31943b64c901dda7ca40
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2019-12-29 09:57:51 -0500

    Setup of app URL and views

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    text_var = 'This is my first Django app web page.'
+    return HttpResponse(text_var)
