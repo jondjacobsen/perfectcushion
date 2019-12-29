5f8c0e4e817a240af7700a80174475f3e41d77a9
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2019-12-29 17:20:27 -0500

    Added First Models, Category and Product

---
+++ shop/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = model.TextField(blank=True)
+    image = models.ImageField(upload_to='category', blank=True)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = model.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product', blank=unique)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str(self):
+        return '{}'.format(self.name)
+
