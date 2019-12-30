3b098afa5c722fd93a90aed32b2504c693a31d26
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2019-12-30 15:43:56 -0500

    Models for Category and Products have been created.

--- shop/models.py
+++ shop/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
-    description = model.TextField(blank=True)
+    description = models.TextField(blank=True)
     image = models.ImageField(upload_to='category', blank=True)
 
     class Meta:
@@ -18,10 +18,10 @@ class Category(models.Model):
 class Products(models.Model):
     name = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
-    description = model.TextField(blank=True)
+    description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='product', blank=unique)
+    image = models.ImageField(upload_to='product', blank=True)
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
