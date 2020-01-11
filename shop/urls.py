5b8c4b211314cc33d1385a2168db3816706609ff
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2020-01-10 22:15:38 -0500

    Category.py file Setup - Troubleshooting Done

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,5 @@ app_name='shop'
 
 urlpatterns = [
     path('', views.allProdCat, name='allProdCat'),
-    path('<slug:c_slug>/', views.allProdCat, name='products_by_category',
+    path('<slug:c_slug>/', views.allProdCat, name='products_by_category',)
 ]
--- shop/views.py
+++ shop/views.py
@@ -11,9 +11,9 @@ def index(request):
 def allProdCat(request, c_slug=None):
     c_page = None
     products = None
-    if c_slug =None:
-        c+page = get_object_or_404(Category, slug=c_slug)
-        products = Products.objects.filter(category=c_page, available=True)
+    if c_slug != None:
+        c_page = get_object_or_404(Category, slug=c_slug)
+        products = Product.objects.filter(category=c_page, available=True)
     else:
-        products = Prodcuts.objects.all().filter(available=True)
-    return render(request, 'shop/category.html', {'category':c_page, 'products':products})
+        products = Product.objects.all().filter(available=True)
+    return render(request, 'shop/category.html', {'category':c_page, 'products': products})
