597fa7136fdcc3609004135e5353cee4453c85ac
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2020-01-10 21:14:37 -0500

    Added Shop Templates

--- perfectcushion/urls.py
+++ perfectcushion/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from shop import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('shop/', include('shop.urls')),
 ]
 
 if settings.DEBUG:
