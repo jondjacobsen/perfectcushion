f4c24f8f9ad720dbf3ded34ab0048d7f68f90505
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2020-02-02 08:29:46 -0500

    Added Pagination.

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse
 from .models import Category, Product
-
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 def index(request):
     text_var = 'This is my first Django app web page.'
@@ -11,12 +11,22 @@ def index(request):
 
 def allProdCat(request, c_slug=None):
     c_page = None
-    products = None
+    products_list = None
     if c_slug != None:
         c_page = get_object_or_404(Category, slug=c_slug)
-        products = Product.objects.filter(category=c_page, available=True)
+        products_list = Product.objects.filter(category=c_page, available=True)
     else:
-        products = Product.objects.all().filter(available=True)
+        products_list = Product.objects.all().filter(available=True)
+#Pagination code
+    paginator = Paginator(products_list, 6)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+    try:
+        products = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
     return render(request, 'shop/category.html', {'category':c_page, 'products': products})
 
 def ProdCatDetail(request, c_slug, product_slug):
