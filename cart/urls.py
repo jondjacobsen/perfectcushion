8f7c0374ba41dda8ab8f723f209ad8c472dfdc42
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2020-02-11 18:23:53 -0500

    Remove items from cart - cart.html

--- cart/urls.py
+++ cart/urls.py
@@ -6,4 +6,5 @@ app_name='cart'
 urlpatterns = [
     path('add/<int:product_id>/', views.add_cart, name='add_cart'),
     path('', views.cart_detail, name='cart_detail'),
+    path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
 ]
