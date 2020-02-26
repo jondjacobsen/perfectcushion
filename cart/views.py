b480200e1f0a4394bf9ef5adea206dd6392abe42
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2020-02-25 22:54:54 -0500

    Created Orders Tables - save

--- cart/views.py
+++ cart/views.py
@@ -54,10 +54,20 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
     data_key = settings.STRIPE_PUBLISHABLE_KEY
     if request.method == 'POST':
       # print(request.POST)
+
       try:
           token = request.POST['stripeToken']
           email = request.POST['stripeEmail']
           billingName = request.POST['stripeBillingName']
+          billingAddress1 = request.POST['stripeBillingAddressLine1']
+          billingCity = request.POST['stripeBillingAddressCity']
+          billingPostCode = request.POST['stripeBillingZip']
+          billingCountry = request.POST['stripeBillingCountryCode']
+          shippingName = request.POST['stripeShippingName']
+          shippingAddress1 = request.POST['stripeShippingAddressLine1']
+          shippingCity = request.POST['stripeShippingAddressCity']
+          shippingPostcode = request.POST['stripeShippingAddressZip']
+          shippingCountry = request.POST['stripeShippingAddressCountryCode']
           customer = stripe.Customer.create(
               email=email,
               source=token
@@ -67,11 +77,40 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
               currency="usd",
               description=description,
               customer=customer.id
-          )
+            )
 
-
+          try:
+            order_details = Order.objects.create(
+                token=token,
+                total=total,
+                  emailAddress=email,
+                  billingName=billingName,
+                  billingAddress1=billingAddress1,
+                  billingCity=billingCity,
+                  billingPostCode=billingPostCode,
+                  billingCountry=billingCountry
+                )
+            order_details.save()
+            for order_item in cart_items:
+                  oi = OrderItem.objects.create(
+                      product=order_item.product.name,
+                      quantity=order_item.quantity,
+                      price=order_item.product.price,
+                      order=order_details
+                    )
+                  oi.save()
+                  # Reduce Stock Level when order is placed or saved
+                  products = Product.objects.get(id=order_item.product_id)
+                  products.stock = int(order_item.product.stock - order_item.quantity)
+                  products.save()
+                  order_item.delete()
+                  # The terminal will print message when the order is saved
+                  print('The Order has been created')
+            return redirect('shop:allProdCat')
+          except ObjectDoesNotExist:
+              pass
       except stripe.error.CardError as e:
-        return False, e
+                return False, e
     return render(request, 'cart.html', dict(cart_items = cart_items, total = total, counter = counter, data_key = data_key, stripe_total = stripe_total, description = description))
 
 
