555edb18887e25875b548dd2663265ab51ebd567
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2020-03-03 11:52:29 -0500

    Created SignUp Form - forms.py

--- shop/forms.py
+++ shop/forms.py
@@ -1,2 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100)
