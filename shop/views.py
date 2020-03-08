eee351c0519cfb001cc580e8172f5a89a755c2ef
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2020-03-07 20:19:12 -0500

    Created the signin.html form - with sign in information

--- shop/views.py
+++ shop/views.py
@@ -4,6 +4,8 @@ from .models import Category, Product
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from django.contrib.auth.models import Group, User
 from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
 
 
 def index(request):
@@ -58,3 +60,18 @@ def signUpView(request):
     return render(request, 'accounts/signup.html', {'form':form})
 
 
+def signinView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('shop:allProdCat')
+            else:
+                return redirect('signup')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/signin.html', {'form':form})
